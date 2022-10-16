73474ed665211b3e4ec5bb5d8879d32a493938cb
Borja <borjarodrilore@gmail.com>
2022-10-16 22:07:09 +0200

    Added me to configuration panel

--- es.uniovi.pulso.colmena/src/es/uniovi/pulso/colmena/presentation/preferences/ColmenaPreferencesPage.java
+++ es.uniovi.pulso.colmena/src/es/uniovi/pulso/colmena/presentation/preferences/ColmenaPreferencesPage.java
@@ -71,8 +71,9 @@ public class ColmenaPreferencesPage extends PreferencePage implements
 		label.setText("Maria Del Puerto Paule Ruiz");
 		label = new Label(parent, SWT.NONE);
 		label.setText("Victor Manuel Alvarez Garcia");
+		label = new Label(parent, SWT.NONE);
+        label.setText("Borja Rodríguez Lorenzo");
 		
-
 		label = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
 		label = new Label(parent, SWT.NONE);
 		label.setText("Official project page");
